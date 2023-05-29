f254864845db919b29155bdef4f009ffe233c1da
WuMing000 <1715049618@qq.com>
2023-05-29 09:35:25 +0800

    优化状态栏，将状态栏变成白色

--- app/src/main/java/com/example/launchproject/utils/CustomUtil.java
+++ app/src/main/java/com/example/launchproject/utils/CustomUtil.java
@@ -224,17 +224,16 @@ public class CustomUtil {
         window = activity.getWindow();
         WindowManager.LayoutParams params = window.getAttributes();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            params.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            params.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE;
             window.setAttributes(params);
 
 
             int uiFlags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR; // hide nav bar; // hide status bar
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION; // hide nav bar; // hide status bar
 
-            uiFlags |= 0x00001000;    //SYSTEM_UI_FLAG_IMMERSIVE_STICKY: hide navigation bars - compatibility: building API level is lower thatn 19, use magic number directly for higher API target level
+            uiFlags |= 0x00001000;    //SYSTEM_UI_FLAG_IMMERSIVE_STICKY: hide navigation bars - compatibility: building API level is lower than 19, use magic number directly for higher API target level
 
             activity.getWindow().getDecorView().setSystemUiVisibility(uiFlags);
         }
