9f6cb69f2709a649b91883e430becfc266cc1db3
WuMing000 <1715049618@qq.com>
2023-06-20 10:04:59 +0800

    修改域名和应用名称

--- app/src/main/java/com/js/launcher/manager/Contact.java
+++ app/src/main/java/com/js/launcher/manager/Contact.java
@@ -2,8 +2,8 @@ package com.js.launcher.manager;
 
 public class Contact {
 
-    public static final String SERVER_URL = "http://fs.arzopa.cn";
-    public static final String FTP_SERVER_IP = "fs.arzopa.cn";
+    public static final String SERVER_URL = "http://fs.gworldtek.com";
+    public static final String FTP_SERVER_IP = "fs.gworldtek.com";
     public static final int FTP_SERVER_PORT = 21;
     public static final String FTP_SERVER_USERNAME = "ftp_usera";
     public static final String FTP_SERVER_PASSWORD = "123456";
